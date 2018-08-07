e38899a82aacb53349e25a599f00ae99d74ea602
RazvanAnisia <anisiarazvan@gmail.com>
2018-08-07 20:50:08 +0100

    added summary, and removing  items functionality

---
+++ calorie_count/src/components/IntakeSummary/IntakeSummary.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+import classes from './IntakeSummary.css';
+
+class intakeSummary extends Component {
+  removeProduct=(productIndex)=>{
+    //Make copy of props
+    const products = [...this.props.productsData];
+    //delete the product from the array
+    products.splice(productIndex,1)
+    //set the state in the parent component
+    this.props.setState({products: products}, this.props.update);
+  }
+  render(){
+    //make copy of state passed as props
+    const productsData = [...this.props.productsData];
+    //map through an create the li's for each product with the data
+    let summary = productsData.map((product,index)=>{
+      return (
+        <li key={index} ><span  className={classes.name}>{product.name}</span><span>{product.quantity}g</span> <span>{product.calories}kcal</span>
+        <button onClick={()=>this.removeProduct(index)}>X</button> </li>
+      )
+    })
+
+    return(
+      <div>
+        <ul className={classes.summary}>
+           {summary}              
+       </ul>
+      </div>
+    )
+  }
+  
+}
+export default intakeSummary;
--- calorie_count/src/containers/CalorieApp/FoodIntake/FoodIntake.js
+++ calorie_count/src/containers/CalorieApp/FoodIntake/FoodIntake.js
@@ -1,5 +1,6 @@
 import React,{Component}  from 'react';
 import classes from './FoodIntake.css';
+import IntakeSummary from '../../../components/IntakeSummary/IntakeSummary';
 //54491472
 class FoodIntake extends Component{
   state={
@@ -9,57 +10,94 @@ class FoodIntake extends Component{
       totalCarbs:null,
       totalProtein:null,
       totalFats:null,
-    }
+    },
+    showIntakeSummary:false
   }
   getQuantityHandler = (e)=>{
     const input = e.target.children[1].value;
     this.calculateIntake(input)
     e.preventDefault();
 
-    let products = [...this.state.products];
-    let name = this.props.product;
-    products.push(name)
-    this.setState({products:products})
-
   }
   calculateIntake = (inp)=>{
      const inpNum = Number(inp);
     //!!!make a copy of the props that were passed into this component
     const nutriments = {...this.props.nutrients};
-    //make a copy of the state in this component
-    const totalCalc = {...this.state.total}
-
+   
     let energy = Number(nutriments.energy_value);
     let carbs = Number(nutriments.carbohydrates_100g);
     let fats = Number(nutriments.fat_100g);
     let protein = Number(nutriments.proteins_100g);
-    console.log(protein, fats)
     //make calculations
     let caloriesCalc = (energy/(100/inpNum)).toFixed(2);
     let carbsCalc = (carbs/(100/inpNum)).toFixed(2);
     let fatsCalc = (fats/(100/inpNum)).toFixed(2);
     let proteinCalc = (protein/(100/inpNum)).toFixed(2)
-
-    console.log(proteinCalc, fatsCalc)
-
-    totalCalc.totalCalories = caloriesCalc;
-    totalCalc.totalCarbs = carbsCalc;
-    totalCalc.totalFats = fatsCalc;
-    totalCalc.totalProtein = proteinCalc;
-
+    
+    //make copy of state
+    const products = [...this.state.products];
+    const name = this.props.product;
+    const newProduct ={}
+    newProduct.name = name;
+    newProduct.calories = caloriesCalc;
+    newProduct.carbs = carbsCalc;
+    newProduct.fats = fatsCalc;
+    newProduct.proteins = proteinCalc;
+    newProduct.quantity = inpNum;
+    products.push(newProduct);
     //set the state to the new state
-    this.setState({total:totalCalc})
+    // this.setState({total:totalCalc}, ()=> this.updateTotal)
+    this.setState({products:products}, this.updateTotal)
   }
 
+  updateTotal=()=>{
+    const total = {...this.state.total};
+    const products = [...this.state.products];
+    console.log(products)
+    let calories = 0;
+    let proteins = 0;
+    let fats = 0;
+    let carbs = 0;
+    products.forEach((el)=>{
+        calories += Number(el.calories);
+        proteins += Number(el.proteins);
+        carbs += Number(el.carbs);
+        fats += Number(el.fats);
+
+      })
+    total.totalCalories = calories.toFixed(2);
+    total.totalProtein = proteins.toFixed(2);
+    total.totalFats = fats.toFixed(2);
+    total.totalCarbs = carbs.toFixed(2);   
+    console.log(total)
+    this.setState({total:total});
+
+  }
+  showSummary =()=>{
+  const show =!this.state.showIntakeSummary;
+  this.setState({showIntakeSummary:show})
+  }
   render(){
     let showInfo;
+    let summary;
+    if(this.state.showIntakeSummary){
+      summary = <IntakeSummary productsData={this.state.products} setState={(p,c)=>this.setState(p,c)} update={this.updateTotal}/>;
+    }else{
+      summary = null;
+    }
     if(this.state.products.length >0){
-      showInfo = (<ul className={classes.info}>
-          <li><span>Calories:</span> {this.state.total.totalCalories}Kcal</li>
-          <li><span>Carbs:</span> {this.state.total.totalCarbs} g</li>
-          <li><span>Protein:</span> {this.state.total.totalProtein}g</li>
-          <li><span>Fats:</span> {this.state.total.totalFats}g</li>
-        </ul>)
+      showInfo = (
+     <div>
+        <ul className={classes.info}>
+            <li><span>Calories:</span> {this.state.total.totalCalories}Kcal</li>
+            <li><span>Carbs:</span> {this.state.total.totalCarbs} g</li>
+            <li><span>Protein:</span> {this.state.total.totalProtein}g</li>
+            <li><span>Fats:</span> {this.state.total.totalFats}g</li>
+        </ul>
+        <button onClick={this.showSummary}>{this.state.showIntakeSummary ? 'Hide Summary' :'Show Summary' }</button>
+        {summary}
+      </div>
+     )
     }else{
       showInfo=null;
     }
